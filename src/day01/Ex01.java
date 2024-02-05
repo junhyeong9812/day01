9d3f7a4b7ee5692fce0663feea586a87af22e30d
jun <pickjog@gmail.com>
2024-02-05 16:59:56 +0900

    2월 5일

---
+++ src/day01/Ex01.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class Ex01 {
+
+public static void main(String[] args) {
+	//main은 프로그램의 시작점
+	//sysout ctrl space자동완성 가능
+	System.out.println(100); //()안에 있는 값을 출력하는 함수
+	//System.out.println(123);
+	//단일 주석
+	/*
+	 * 여러줄 주석
+	 * */
+	System.out.println("내용");
+	System.out.println("내용");
+	System.out.println("내용");
+	//ctrl alt (아래방향키)누르면 1줄이 복사가 된다.
+	System.out.print(123);
+	System.out.print(123);
+	System.out.print(123);
+	//ln이 없다면 붙여써진다.
+}
+
+}
---
+++ src/day01/Ex02.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex02 {
+public static void main(String[] args) {
+	System.out.println("100\n200\n300\n");
+	System.out.println("123456781");
+	System.out.println("1\t1");
+	System.out.println("이름\t나이");
+	System.out.println("============");
+	System.out.println("홍길동\t20살");
+	System.out.println("김개동\t30살");
+	System.out.println("============");
+	System.out.println("D:\\핀테크\\workspace-java\\test-home\\test");
+	System.out.println("안녕하\"세요");
+	
+}
+}
